902c9bdc58e99bf6f2fd727a9170a35235fb87f3
william <gatesyyy@gmail.com>
2024-11-22 18:10:52 +0800

    update weibofeed
    18点10分

--- rss/generate_content.py
+++ rss/generate_content.py
@@ -270,10 +270,16 @@ import os
 import re
 
 # 定义RSS URL
-rss_urls = {
-    'user1': 'https://rsshub.app/weibo/user/1659643027',
-    'user2': 'https://rsshub.app/weibo/user/1253846303'
-}
+# rss_urls = {
+#     'user1': 'https://rsshub.app/weibo/user/1659643027',
+#     'user2': 'https://rsshub.app/weibo/user/1253846303'
+# }
+
+# Define a list of user IDs as strings
+user_ids_base = ['1694917363', '2522334710', '2214838982', '1655747731', '1649111590', '7746281422', '1881320895', '1404521940', '1260797924', '1432187644', '7188823772', '1240212845', '1668726803', '1941998575', '1912273717', '5283695116', '2213561393', '5103458366', '1454560380', '5692692520', '3867285047', '2194035935', '2987585965', '1778375693', '1659643027', '5466550668', '7217947278', '1670743152', '6827625527', '1092211747', '6048569942', '3138279871', '2400966427', '5722964389', '1727858283', '7416690461', '1253846303', '5955106173']
+user_ids = set(user_ids_base);
+# Generate rss_urls dictionary dynamically
+rss_urls = {f'user{i+1}': f'https://rsshub.app/weibo/user/{user_id}' for i, user_id in enumerate(user_ids)}
 
 # 定义请求头
 headers = {
@@ -281,7 +287,7 @@ headers = {
 }
 
 # 定义模板字符串
-template_str = '''
+""" template_str = '''
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -295,6 +301,27 @@ template_str = '''
         <h2><a href="{{ entry.link }}">{{ entry.title }}</a></h2>
         <p>{{ entry.description }}</p>
         <p>发布时间: {{ entry.published }}</p>
+    </div>
+    {% endfor %}
+</body>
+</html>
+''' """
+
+template_str = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>微博汇总</title>
+    <link rel="stylesheet" href="lasteststyles.css">
+</head>
+<body>
+    <h1>微博汇总</h1>
+    {% for entry in entries %}
+    <div>
+        <h2><a href="{{ entry.link }}">{{ entry.title }}</a></h2>
+        <p>{{ entry.description }}</p>
+        <p class="published">发布时间: {{ entry.published }}</p>
     </div>
     {% endfor %}
 </body>
@@ -314,7 +341,14 @@ all_entries = []
 
 for user, url in rss_urls.items():
     r = requests.get(url, headers=headers)
-    tree = etree.fromstring(r.text.encode('utf-8'))
+    """ tree = etree.fromstring(r.text.encode('utf-8')) """
+    # 清洗RSS内容，确保所有&符号都被正确转义
+    content = r.text.replace('&', '&amp;').replace('&amp;amp;', '&amp;')
+    try:
+        tree = etree.fromstring(content.encode('utf-8'))
+    except etree.XMLSyntaxError as e:
+        print(f"XML解析错误: {e}")
+        continue
     entries = tree.xpath('//item')
     for entry in entries:
         title = entry.find('title').text
