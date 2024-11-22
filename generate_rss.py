173936ab835423280606b3af92a002fcb63f0f29
william <silinge@users.noreply.github.com>
2024-11-22 13:30:33 +0800

    Update generate_rss.py
    13点30分

--- generate_rss.py
+++ generate_rss.py
@@ -1,18 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+import hashlib
+import os
 
-# 微博用户ID列表
-user_ids = ['2269443552', '1253846303', 'Emma古小月', '1659643027']
+# 微博用户ID
+user_id = '1659643027'
 
 # 生成RSS文件
-def generate_rss(user_ids):
+def generate_rss(user_id):
+    url = f'https://weibo.com/u/{user_id}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
     rss_content = '''<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
-    <title>微博订阅源</title>
-    <link>https://silinge.github.io</link>
-    <description>微博订阅源</description>
+    <title>{user_id} 的订阅源</title>
+    <link>{url}</link>
+    <description>{user_id} 的最新微博</description>
     <atom:link href="https://silinge.github.io/rss/feed.xml" rel="self" type="application/rss+xml" />
     <language>zh-cn</language>
     <lastBuildDate>{last_build_date}</lastBuildDate>
@@ -23,13 +29,7 @@ def generate_rss(user_ids):
     last_build_date = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
     pub_date = last_build_date
 
-    for user_id in user_ids:
-        url = f'https://weibo.com/u/{user_id}'
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # 这里需要解析微博内容，具体解析方式取决于微博页面的结构
-       # 解析微博内容
+    # 解析微博内容
     weibo_items = soup.find_all('div', class_='WB_cardwrap WB_feed_type S_bg2')
 
     for item in weibo_items:
@@ -59,5 +59,24 @@ def generate_rss(user_ids):
     with open('rss/feed.xml', 'w', encoding='utf-8') as f:
         f.write(rss_content)
 
-# 生成RSS文件
-generate_rss(user_ids)
+    return rss_content
+
+# 检查文件内容是否发生变化
+def has_file_changed(new_content):
+    if not os.path.exists('rss/feed.xml'):
+        return True
+
+    with open('rss/feed.xml', 'r', encoding='utf-8') as f:
+        old_content = f.read()
+
+    return hashlib.md5(old_content.encode()).hexdigest() != hashlib.md5(new_content.encode()).hexdigest()
+
+# 生成RSS文件并检查是否需要提交
+new_rss_content = generate_rss(user_id)
+if has_file_changed(new_rss_content):
+    with open('rss/feed.xml', 'w', encoding='utf-8') as f:
+        f.write(new_rss_content)
+    print("RSS feed updated and will be committed.")
+else:
+    print("RSS feed has not changed. No commit needed.")
+    exit(0)  # 如果没有更改，退出脚本
